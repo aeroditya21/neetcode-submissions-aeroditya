817f8ad783ce99535895055c5456fb8f59fda818
Aditya Kanade <aeroditya@gmail.com>
2026-04-18 20:41:52 -0700

    Add: contains-duplicate-ii - submission-3

---
+++ Data Structures & Algorithms/contains-duplicate-ii/submission-3.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        if (k == 0) return false;
+        Map<Integer, Integer> indexMap = new HashMap<>();
+        for (int i=0; i<nums.length; i++) {
+            if (indexMap.containsKey(nums[i]) && ((i-indexMap.get(nums[i])) <= k)) {
+                return true;
+            } else {
+                indexMap.put(nums[i], i);
+            }
+            //System.out.println(indexMap);
+        }
+        return false;
+    }
+}
